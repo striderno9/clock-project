982e3a0f20745f578282f8faa61c37bd435e0361
Jason Almenas <i.striderno9@icloud.com>
2016-07-12 19:26:19 -0400

    created files

---
+++ script.js
@@ -0,0 +1,54 @@
+// change css using jquery
+// $("#txt").css({ color: "blue", fontSize: "25px" });
+
+// 4 / 2 = 2
+// 4 / 3 = 1 R1
+
+// 4 % 2 = 0
+// 4 % 3 = 1
+
+// 2 % 2 = 0
+// 3 % 2 = 1
+
+// if a number is even then % with 2 will return 0
+// otherwise if a number is odd then % with 2 will return 1
+
+$(document).ready(function() {
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var ampm = amOrPm(h);
+    h = convertHours(h);
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    $("#txt").html(h + ":" + m + ":" + s + ampm)
+    setTimeout(startTime, 500);
+  }
+
+  function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i
+    }
+    return i;
+  }
+
+  function convertHours(i) {
+    if (i > 12) {
+        i = i - 12;
+    }
+    return i;
+  }
+
+  function amOrPm(i) {
+    if (i < 12) {
+        return "AM";
+    } else {
+        return "PM";
+    }
+  }
+
+  startTime();
+});
